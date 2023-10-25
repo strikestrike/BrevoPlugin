4ff1a18cf09646ab056915eda2066372eb917c24
strikestrike <newssstrike@gmail.com>
2023-10-25 13:05:59 +0200

    super/feat: settings ui implemented

--- brevo-integration/brevo-integration.php
+++ brevo-integration/brevo-integration.php
@@ -3,15 +3,49 @@
 Plugin Name: Brevo Integration
 Description: Custom integration between WooCommerce and Brevo's API.
 Version: 1.0
-Author: Your Name
+Author: Super0312
 */
 
 // Include the Brevo API SDK
 require_once(__DIR__ . '/vendor/autoload.php');
 
-// Hook into WooCommerce events to trigger your integration
-add_action('woocommerce_new_order', 'brevo_integration');
 
+// Step 1: Define the plugin settings
+function brevo_integration_settings() {
+    add_option('brevo_api_key', ''); // API Key
+    add_option('brevo_contact_list_id', ''); // Contact List ID
+}
+
+// Step 2: Create the settings page
+function brevo_integration_settings_page() {
+    add_options_page('Brevo Integration Settings', 'Brevo Integration', 'manage_options', 'brevo_integration_settings', 'brevo_integration_settings_form');
+}
+
+// Step 3: Display and Process the settings
+function brevo_integration_settings_form() {
+    if (isset($_POST['submit'])) {
+        update_option('brevo_api_key', $_POST['brevo_api_key']);
+        update_option('brevo_contact_list_id', $_POST['brevo_contact_list_id']);
+        echo '<div class="updated"><p>Settings saved</p></div>';
+    }
+    
+    echo '<div class="wrap">';
+    echo '<h2>Brevo Integration Settings</h2>';
+    echo '<form method="post" action="">';
+    
+    echo '<label for="brevo_api_key">API Key:</label>';
+    echo '<input type="text" name="brevo_api_key" value="' . get_option('brevo_api_key') . '"><br>';
+    
+    echo '<label for="brevo_contact_list_id">Contact List ID:</label>';
+    echo '<input type="text" name="brevo_contact_list_id" value="' . get_option('brevo_contact_list_id') . '"><br>';
+    
+    echo '<input type="submit" name="submit" class="button button-primary" value="Save Settings">';
+    
+    echo '</form>';
+    echo '</div>';
+}
+
+// contact integration.
 function brevo_integration($order_id) {
     // Get the API key from the wp-config.php file
     $api_key = defined('BREVO_API_KEY') ? BREVO_API_KEY : 'YOUR_API_KEY';
@@ -56,3 +90,11 @@ function brevo_integration($order_id) {
         error_log('Exception when calling ContactsApi->createContact: ' . $e->getMessage());
     }
 }
+
+
+// Add the settings page and settings initialization
+add_action('admin_menu', 'brevo_integration_settings_page');
+add_action('admin_init', 'brevo_integration_settings');
+
+// Hook into WooCommerce events to trigger your integration
+add_action('woocommerce_new_order', 'brevo_integration');
